4f5f1e5fc5a2e0eec598024d8487ed90a2f6eb9e
arccosine <40669+ArcCosine@users.noreply.github.com>
2020-04-10 16:21:25 +0900

    [Update] Update google's html changes.

--- content.js
+++ content.js
@@ -1,9 +1,16 @@
-(()=>{
-    let _arrow, _arrowPos = -1;
+(() => {
+    let arrow;
+    let _arrowPos = -1;
 
-    // TODO
     // This _selector may change on a regular basis every time Google's HTML changes.
-    const _selector = '.r > a,#pnprev,#pnnext';
+    const _selector = ".r > a,#pnprev,#pnnext";
+
+    // append fake arrow
+    const appendArrow = () => {
+        _arrow = document.body.appendChild(document.createElement("div"));
+        _arrow.classList.add("fake-arrow");
+        down();
+    };
 
     const up = () => {
         move(1);
@@ -13,69 +20,80 @@
         move(-1);
     };
 
-    const isInView = (node) => {
-        const rect = node.getBoundingClientRect();
-        return node === document.elementFromPoint(rect.left, rect.top);
-    };
+    const moveArrow = node => {
+        const appendNode = node.parentNode;
 
-    const moveArrow = (node) => {
-        if( !_arrow ){
-            _arrow = document.body.appendChild(document.createElement("div"));
-            _arrow.classList.add("fake-arrow");
+        if (node.id === "pnnext" || node.id === "pnprev") {
+            appendNode.style.position = "relative";
         }
-        if( node.id === "pnnext" ){
+
+        if (node.id === "pnnext") {
             _arrow.classList.add("right");
         } else {
             _arrow.classList.remove("right");
         }
-        const appendNode = node.id.indexOf("pn") > -1 ? node.parentNode : node.parentNode.parentNode.parentNode.parentNode;
-        appendNode.style.position = "relative";
+
         appendNode.appendChild(_arrow);
-        if(!isInView(appendNode)){
-            window.scrollTo({
-                top: appendNode.offsetTop,
-                behavior: 'smooth'
-            });
-        }
+
+        const headerHeight = document
+            .querySelector(".sfbg")
+            .getBoundingClientRect().height;
+        const mypos =
+            window.pageYOffset +
+            appendNode.getBoundingClientRect().top -
+            headerHeight;
+
+        window.scrollTo({
+            top: mypos,
+            behavior: "smooth"
+        });
     };
-    const move = (no) => {
+    const move = no => {
         const nodes = document.querySelectorAll(_selector);
-        const nextPos = _arrowPos+no;
-        const pos = nextPos < 0 ? 0 : nextPos > nodes.length-1 ? nodes.length-1 : nextPos;
+        const nextPos = _arrowPos + no;
+        const pos =
+            nextPos < 0
+                ? 0
+                : nextPos > nodes.length - 1
+                ? nodes.length - 1
+                : nextPos;
         moveArrow(nodes[pos]);
         _arrowPos = pos;
     };
 
-    const open = (keyState) => {
+    const open = keyState => {
         const nodes = document.querySelectorAll(_selector);
-        if( keyState.ctrl === true ){
+        if (keyState.ctrl === true) {
             nodes[_arrowPos].setAttribute("target", "_blank");
             nodes[_arrowPos].setAttribute("rel", "noopener");
         } else {
             nodes[_arrowPos].removeAttribute("target");
         }
-        if( keyState.shift === true ){
-            nodes[_arrowPos]
-            chrome.runtime.sendMessage({"action":"background","url":nodes[_arrowPos].href}, (response) => {
-                //do response
-            });
+        if (keyState.shift === true) {
+            nodes[_arrowPos];
+            chrome.runtime.sendMessage(
+                { action: "background", url: nodes[_arrowPos].href },
+                response => {
+                    //do response
+                }
+            );
         } else {
             nodes[_arrowPos].click();
         }
     };
 
     const focus = () => {
-        _arrowPos = -1;//reset
+        _arrowPos = -1; //reset
         document.querySelector(".gLFyf.gsfi").focus();
     };
 
     // control
-    const keyHandler = (event) => {
+    const keyHandler = event => {
         //console.log(event.key);
         const handleObj = {
-            "Enter" : open,
-            "j": up,
-            "k": down,
+            Enter: open,
+            j: up,
+            k: down,
             "/": focus
         };
         if (event.defaultPrevented) {
@@ -83,7 +101,11 @@
         }
 
         const node = event.target;
-        if( node.nodeType === 1 && !/INPUT|TEXTAREA/.test(node.tagName.toUpperCase()) && typeof handleObj[event.key]  === "function"){
+        if (
+            node.nodeType === 1 &&
+            !/INPUT|TEXTAREA/.test(node.tagName.toUpperCase()) &&
+            typeof handleObj[event.key] === "function"
+        ) {
             event.preventDefault();
             handleObj[event.key]({
                 ctrl: event.ctrlKey,
@@ -92,7 +114,7 @@
         }
     };
 
-
     // Entry Point
-    document.addEventListener("keydown", keyHandler, false );
+    document.addEventListener('DOMContentLoaded', appendArrow, false );
+    document.addEventListener("keydown", keyHandler, false);
 })();
